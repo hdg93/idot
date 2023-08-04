699a2c0578d2368e7579e293926f9ef6aefce75e
hdg93 <hdg93kr@naver.com>
2023-08-04 21:51:05 +0900

    module by yolov5

--- export.py
+++ export.py
@@ -152,7 +152,7 @@ def export_torchscript(model, im, file, optimize, prefix=colorstr('TorchScript:'
 def export_onnx(model, im, file, opset, dynamic, simplify, prefix=colorstr('ONNX:')):
     # YOLOv5 ONNX export
     check_requirements('onnx>=1.12.0')
-    import onnx
+    
 
     LOGGER.info(f'\n{prefix} starting export with onnx {onnx.__version__}...')
     f = file.with_suffix('.onnx')
